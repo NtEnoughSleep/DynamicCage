004bbf5be1e3b84484b6b5eb58998dd2dda58641
sajjad soleimani <sajjadsoleimani1384@gmail.com>
2024-12-08 16:56:50 +0330

    add custom object for blocks

---
+++ src/main/java/ir/Awake/BlockKey.java
@@ -0,0 +1,38 @@
+package ir.Awake;
+
+import java.util.Objects;
+
+public class BlockKey {
+    private final int x;
+    private final int y;
+    private final int z;
+    public double getX() {
+        return (double) x;
+    }
+
+    public double getY() {
+        return (double) y;
+    }
+
+    public double getZ() {
+        return (double) z;
+    }
+
+    public BlockKey(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        BlockKey blockKey = (BlockKey) obj;
+        return x == blockKey.x && y == blockKey.y && z == blockKey.z;
+    }
+}
